6e4a2e40da60cdf0d32dc9b15fc479e816775dda
Alex Adusei <alex.adusei@gmail.com>
2016-09-17 14:37:39 -0400

    Finalized API endpoints

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,3 +5,12 @@ var apiRoutes = express.Router();
 apiRoutes.get('/', function(req, res) {
     res.json({ message: 'Welcome to this API! It is at http://localhost:8080/api' });
 });
+
+apiRoutes.post('/getcoords', function(req, res) {
+  var encodedUrl = req.body.encoded_url;
+
+  res.json({ message: "Here's the response: " + encodedUrl })
+
+})
+
+module.exports = apiRoutes;
