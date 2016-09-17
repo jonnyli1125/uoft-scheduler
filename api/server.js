b542f2c42dc1fdc41eef5a2bde59c91444eca314
Alex Adusei <alex.adusei@gmail.com>
2016-09-17 14:06:51 -0400

    Added dependencies and express skeleton

---
+++ api/server.js
@@ -0,0 +1,30 @@
+// get the packages we need
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+// configuration
+var port = process.env.PORT || 8080;
+
+// routes
+// var apiRoutes = require('./app/api/api.js');
+// var authRoutes = require('./app/api/auth');
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to lo requests to the console
+app.use(morgan('dev'));
+
+// ~~~~~~~~~~~ Routes ~~~~~~~~~~~
+
+// basic route
+app.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the main route! The API is at http:localhost:' + port + '/api' });
+});
+
+// start the server
+app.listen(port);
+console.log('Server running at http://localhost:' + port);
