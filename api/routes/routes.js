51a177d66c07e3c14397b1493a832c47199c7808
Alex Adusei <alex.adusei@gmail.com>
2016-09-17 23:18:46 -0400

    Twerking dataset dependencies

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var apiRoutes = express.Router();
+var path = require('path');
+var courses = require(path.join(__dirname, '../', '../', './datasets/courses'));
+
+// var Post = require(path.join(__dirname, '../', './models/post'));
 
 // basic API route
 apiRoutes.get('/', function(req, res) {
@@ -7,11 +11,21 @@ apiRoutes.get('/', function(req, res) {
 });
 
 apiRoutes.post('/getcoords', function(req, res) {
+  // var courses = JSON.parse(courses)
   var encodedUrl = req.query.encoded_url;
   var day = req.query.day;
   var term = req.query.term;
 
-  var decodedCourses = new Buffer(encodedUrl, 'base64').toString();
+  var decodedCourses = new Buffer(encodedUrl, 'base64').toString().split(',');
+  var targetCourse;
+
+  console.log("Decoded courses:", decodedCourses)
+
+  decodedCourses.map(function(decCourse) {
+    console.log("Course: ", decCourse)
+  })
+
+  // console.log("ALL THE COURSES IN THE WORLD: ", courses)
 
   res.json({ message: "Here's the response: " + decodedCourses })
 
